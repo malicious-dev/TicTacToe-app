816cf0f017057b25e5374523cb137629ea6330e8
Malicious-dev <Programming1hars@gmail.com>
2023-12-04 04:33:56 +0530

    Add background image and styling to App.js

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import bg from './assets/bg.jpeg';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={ styles.container }>
+      <ImageBackground source={ bg } style={ styles.bg } resizeMode='contain'>
+        <View style={ styles.circle }>
+
+        </View>
+        <View>
+          <View style={ [styles.crossLine, styles.crossLineReversed] } />
+          <View style={ styles.crossLine } />
+        </View>
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +25,35 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#242D34'
   },
+  bg: {
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 15
+  },
+  circle: {
+    width: 75,
+    height: 75,
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 10,
+    borderWidth: 10,
+    borderColor: 'white',
+  },
+
+  crossLine: {
+    position: 'absolute',
+    width: 10,
+    height: 70,
+    backgroundColor: "white",
+    borderRadius: 5,
+    transform: [{ rotate: '45deg' }]
+  },
+  crossLineReversed: {
+    transform: [{ rotate: '-45deg' }]
+  }
 });
